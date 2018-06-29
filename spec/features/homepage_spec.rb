9153008daf7e3c70f1eb756a0138e803ec851491
Sebastian Nepote <sebastian.nepote@helpling.com>
2018-06-29 11:16:21 +0200

    cleanup home spec feature

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -1,14 +1,7 @@
 require 'rails_helper'
 
-# feature "Homepage" do
-#   scenario "As a user I go to see the home title" do
-#     visit '/'
-#     expect(page).to have_content 'Home'
-#   end
-# end
-
 feature 'As a user I go to homepage' do
-  describe 'logo', type: :system do
+  describe 'homepage', type: :system do
     it 'shows the home text' do
       visit '/'
       expect(page).to have_content('Home')
